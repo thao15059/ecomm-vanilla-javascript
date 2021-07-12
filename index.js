e696ded7ba2c4c59f49d92cb53a9456e2993b79c
thao15059 <thao15059@gmail.com>
2021-07-12 14:52:15 +0700

    implements users repository and save data in file

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" type="email" placeholder="Email" />      
-        <input name="password" type="password" placeholder="Password" />      
-        <input name="passwordConfirmation" type="password" placeholder="Password Confirmation" />
-        <input type="submit" value="Sign Up" /> 
-      </form>
-    </div>
-  `);
-});
-
 // const bodyParser = (req, res, next) => {
 //   if (req.method !== "POST") {
 //     next();
@@ -34,6 +21,19 @@ app.get("/", (req, res) => {
 //     next();
 //   });
 // };
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" type="email" placeholder="Email" />      
+        <input name="password" type="password" placeholder="Password" />      
+        <input name="passwordConfirmation" type="password" placeholder="Password Confirmation" />
+        <input type="submit" value="Sign Up" /> 
+      </form>
+    </div>
+  `);
+});
 
 app.post("/", (req, res) => {
   console.log("Form data ", req.body);
