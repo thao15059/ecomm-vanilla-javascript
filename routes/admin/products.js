b898016b71f778eea98b232e9c820bc07bb8d4f8
thao15059 <thao15059@gmail.com>
2021-07-14 12:37:44 +0700

    implements products template, image upload with multer

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
 
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -13,12 +15,20 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  console.log(errors);
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
 
-  res.send("submitted");
-});
+    await productsRepo.create({ title, price, image });
+
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
